8237c6bad7a8898f1c61452871e88307ea3d6e84
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2020-06-02 22:14:21 +0200

    EX-3: Add Extensions classes

---
+++ FileAlphanumericCounter/FileAlphanumericCounter/Extensions/StringExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FileAlphanumericCounter
+{
+    public static class StringExtension
+    {
+        public static IEnumerable<(char Character, int Count)> GetCharactersCount(this string content)
+        {
+            return content.GroupBy(c => c)
+                .Select(c => (Character: c.Key, Count: c.Count()))
+                .OrderByDescending(t => t.Count);
+        }
+    }
+}
