8fd9e7ac57d1aee1a250be84799be9b726c5cb81
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2020-06-03 21:45:36 +0200

    EX-4: Add TupleExtension class

---
+++ ListShuffling/ListShuffling/Extension/TupleExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListShuffling
+{
+    public static class TupleExtension
+    {
+        public static List<string> ShuffleOneByOne(this (List<string>, List<string>) tuple)
+        {
+            List<string> shuffledList = new List<string>();
+            tuple.Item1.Zip(tuple.Item2, (s, s1) => new[] {s, s1}).ToList().ForEach(a => shuffledList.AddRange(a));
+            var longerArray = tuple.Item1.Count > tuple.Item2.Count ? tuple.Item1 : tuple.Item2;
+            shuffledList.AddRange(longerArray.Skip(shuffledList.Count / 2));
+            return shuffledList;
+        }
+
+        public static List<string> RandomShuffle(this (List<string>, List<string>) tuple)
+        {
+            return tuple.Item1.Concat(tuple.Item2).OrderBy(e => Guid.NewGuid()).ToList();
+        }
+    }
+}
