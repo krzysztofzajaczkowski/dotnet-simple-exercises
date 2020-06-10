abea453300cd32762de4cc24537b622c0076c3e2
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2020-06-10 18:28:42 +0200

    EX-7: Add models types

---
+++ SimpleJoins/SimpleJoins/Purchase.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpleJoins
+{
+    public class Purchase
+    {
+        public int InvoiceNo { get; set; }
+        public int ItemId { get; set; }
+        public int PurchaseQuantity { get; set; }
+    }
+}
