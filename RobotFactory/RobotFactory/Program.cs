cef2a065b56fc922746c10db5a6a36533c1e9baa
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2020-06-15 22:07:39 +0200

    EX-9: Add main Program class

---
+++ RobotFactory/RobotFactory/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace RobotFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = RobotFactory.Instance;
+            var robots = new List<Robot>();
+            var random = new Random();
+
+            var numberOfRobots = random.Next(10, 20);
+            for (int i = 0; i < numberOfRobots; i++)
+            {
+                robots.Add(factory.CreateRobot());
+            }
+
+            Console.WriteLine("Created robots");
+            robots.ForEach(Console.WriteLine);
+
+            Console.WriteLine("\nFirst and last robot before/after reset");
+            Console.WriteLine(robots[0].ToString());
+            factory.ResetRobot(robots[0]);
+            Console.WriteLine(robots[0].ToString());
+            
+            Console.WriteLine();
+            
+            Console.WriteLine(robots[numberOfRobots - 1].ToString());
+            factory.ResetRobot(robots[numberOfRobots - 1]);
+            Console.WriteLine(robots[numberOfRobots - 1].ToString());
+
+            Console.WriteLine($"\nAfter creating {numberOfRobots} robots, and resetting two of these, total number of stored identifiers should be {numberOfRobots+2}");
+            Console.WriteLine($"Total number of stored identifiers is {factory.CreatedRobotsNames.Count}");
+
+        }
+    }
+}
